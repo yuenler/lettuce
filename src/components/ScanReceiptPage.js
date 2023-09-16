96beae4b108bd0b9712ae5fcd693115955e039fb
Yuen Ler Chow <lerchow@gmail.com>
2023-09-16 17:51:21 -0400

    dione

--- src/components/ScanReceiptPage.js
+++ src/components/ScanReceiptPage.js
@@ -1,24 +1,29 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { getDocs, collection } from 'firebase/firestore'
-import { auth, firestore } from '../firebase';
-import generic_food from '../assets/generic-food.csv';
-import walmart from '../assets/walmart';
 import { Link, useLocation } from 'react-router-dom';
-import { getDocs, doc, setDoc, deleteDoc, collection } from 'firebase/firestore'
 import { firestore } from '../firebase';
+import walmart from '../assets/walmart';
+import { doc, setDoc, deleteDoc, collection, getDocs } from 'firebase/firestore'
 
 
 const foodLifespan = {
   'zucchini': 5,
   'potato': 10,
+  'milk': 7,
+  'grape': 3,
+  'salad': 7,
+}
+
+const receiptTextToFoodMap = {
+  'ZUCCHINI': 'zucchini',
+  'POTATO': 'potato',
+  'MILK': 'milk',
+  'GRAPE': 'grape',
+  'SALAD': 'salad',
 }
 
 
 const ScanReceiptPage = () => {
   const [imageFile, setImageFile] = useState(null);
-  const [extractedText, setExtractedText] = useState('');
   const location = useLocation();
   const { username } = location.state;
   const [loading, setLoading] = useState(false);
@@ -54,114 +59,20 @@ const ScanReceiptPage = () => {
     }
     getReceipts()
   }
-    , [])
+    , [loading])
 
   async function extractFoodItems(text) {
-    //const foodItems = await parseCSVAndExtractFirstColumn(generic_food);
-    const foodItems = await extractNamesFromJSONString(walmart);
-    //onsole.log(foodItems);
-    function separateTextByNumbers(inputText) {
-      // Use a regular expression to split the text by numbers
-      const textArray = inputText.split(/\d+/);
-    
-      // Filter out any empty strings from the result
-      const filteredArray = textArray.filter((text) => text.trim() !== '');
-    
-      return filteredArray;
-    }
-    const filteredFoods = filterArrayByString(foodItems,text);
-    text = separateTextByNumbers(text);
-    //console.log(text);
-    console.log(filteredFoods);
-    // const foodItems = [];
-    // const lines = text.split(/\r?\n/); // Split the text into lines
+    text = text.toLowerCase();
 
-    // for (const line of lines) {
-    //   // Look for lines that start with a code (e.g., "F") followed by a description
-    //   const match = line.match(/^\s*([A-Z]+\s+[A-Z]+)\s+(.+)/);
-    //   if (match) {
-    //     const code = match[1].trim();
-    //     const description = match[2].trim();
-    //     foodItems.push(description);
-    //   }
-    // }
-
-    return filteredFoods;
-  }
-
-  async function extractNamesFromJSONString(jsonPath) {
-    try {
-      const jsonString = await readFile(jsonPath);
-      const jsonObjects = jsonString.split('\n').filter((line) => line.trim() !== '');
-    
-      // Initialize an array to store the extracted names
-      const allNames = [];
-
-      // Iterate through each JSON object
-      for (const jsonObject of jsonObjects) {
-        const parsedJSON = JSON.parse(jsonObject);
-
-        // Check if the parsed object has an "items" property
-        if (parsedJSON.items && Array.isArray(parsedJSON.items)) {
-          // Extract "name" attributes from each item
-          const itemNames = parsedJSON.items.map((item) => item.name);
-          
-          // Concatenate item names to the result array
-          allNames.push(...itemNames);
-        }
+    const foods = []
+    Object.keys(receiptTextToFoodMap).forEach(key => {
+      if (text.includes(key.toLowerCase())) {
+        foods.push(receiptTextToFoodMap[key])
       }
+    })
 
-      return allNames;
-    } catch (error) {
-      console.error('Error:', error);
-      return [];
-    }
+    return foods;
   }
-
-  function filterArrayByString(array, searchString) {
-    // Convert the searchString to lowercase for case-insensitive comparison
-    searchString = searchString.toLowerCase();
-    console.log(searchString);
-  
-    // Use the filter method to keep items that appear in the searchString
-    const filteredArray = array.filter((item) => {
-      // Convert the item to lowercase for case-insensitive comparison
-      const lowercasedItem = item.toLowerCase();
-      return searchString.split(" ").includes(lowercasedItem);
-    });
-  
-    return filteredArray;
-  }
-  
- // Function to read a CSV file
-async function readFile(file) {
-  const get = await fetch(file).then(
-    (r)=> r.text());
-  
-    return get;
-
-}
-
-  async function parseCSVAndExtractFirstColumn(filePath) {
-    try {
-      const csvData = await readFile(filePath);
-      //console.log(csvData);
-      // Split the CSV data into rows
-      const rows = csvData.split('\n');
-  
-      // Extract the first column from each row
-      const firstColumn = rows.map((row) => {
-        const columns = row.split(',');
-        return columns[0]; // Extract the first element of each row
-      });
-  
-      return firstColumn;
-    } catch (error) {
-      console.error('Error:', error);
-      return null;
-    }
-  }
-  
 
 
   const handleImageChange = (e) => {
@@ -180,17 +91,11 @@ async function readFile(file) {
 
 
     try {
-      // const formData = new FormData();
-      // formData.append('file', imageFile);
       const ReadText = require('text-from-image')
 
       const text = await ReadText(
         imageFile
       )
-
-
-      // const extractedText =
-      //   response?.data?.responses[0]?.fullTextAnnotation?.text || '';
 
       const foods = await extractFoodItems(text);
 
@@ -238,10 +143,6 @@ async function readFile(file) {
             <Link to="/recipes" className="btn btn-primary btn-lg m-3">
               Recipes
             </Link>
-          </div>
-          <div>
-            <h4>Extracted Text:</h4>
-            <p>{extractedText}</p>
           </div>
 
           <div>
